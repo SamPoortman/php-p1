2cff2356952ffa8360a80549cb6ef486021c8503
SamPoortman <9024940@student.zadkine.nl>
2024-12-13 13:45:25 +0100

    e

---
+++ opdracht 5/dobbelsteen.php
@@ -0,0 +1,8 @@
+<?php 
+$score = rand(1,6)
+if ($score = >= 4){
+    $resultaat = "gefeliciteerd je hebt gewonnen"
+}else {
+    $resultaat = "je hebt verloren"
+}
+?>
---
+++ opdracht 5/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>dobbelsteen</h1>
+    <?php include "dobbelsteen.php" ?>
+
+    <p>het cijfer is <?php echo $score ?> </p>
+    <p><?php echo $money</p>
+
+</body>
+</html>
