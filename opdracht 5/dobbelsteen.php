b45b5f445fd9229eff738e7b9a0ea099fe15781d
SamPoortman <9024940@student.zadkine.nl>
2024-12-27 20:27:06 +0100

    e

--- opdracht 5/dobbelsteen.php
+++ opdracht 5/dobbelsteen.php
@@ -1,8 +1,23 @@
-<?php 
-$dobbelsteen = rand(1,6)
-if ($dobbelsteen = >= 4){
-    $resultaat = "gefeliciteerd je hebt gewonnen"
-}else {
-    $resultaat = "je hebt verloren"
+<?php
+$worp = rand(1, 6);
+if ($worp === 6) {
+    $resultaat = "Je hebt een 6 gegooid! Je wint een speciale prijs!";
+} elseif ($worp >= 4) {
+    $resultaat = "Je hebt een $worp gegooid! Je wint het spel!";
+} else {
+    $resultaat = "Je hebt een $worp gegooid! Je verliest het spel.";
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dobbelsteen</title>
+</head>
+<body>
+    <h1>Dobbelsteen</h1>
+    <p><?php echo $resultaat; ?></p>
+</body>
+</html>
